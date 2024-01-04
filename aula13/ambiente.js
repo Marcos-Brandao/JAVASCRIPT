37a3f9e1fbc2d252017513cf1befb3a1c8eee43f
Marcos Brandão <mwellersonbrandao@outlook.com>
2024-01-04 13:34:14 -0300

    repetições

---
+++ aula13/ambiente.js
@@ -0,0 +1,20 @@
+var c = 1 
+ do {
+    console.log(`Passo ${c}`) //Faça enquanto
+    c++
+} while (c <= 6)
+
+/*
+while (c <= 6) {
+    console.log(`Passo ${c}`) //Enquanto
+    c++
+*/
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
