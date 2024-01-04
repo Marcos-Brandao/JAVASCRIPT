2ef354ac881a22301b18296a6c8cc84b4d61ee2b
Marcos Brandão <mwellersonbrandao@outlook.com>
2024-01-04 02:13:30 -0300

    Script terminado-falta imagens

---
+++ ex015/script.js
@@ -0,0 +1,40 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || Number (fano.value) > ano) {
+        window.alert('[ERRO] verifique os dadose tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >= && idade < 10) {
+                // Criança
+            } else if (idade < 21) {
+                // Jovem
+            } else if (idade < 50) {
+                // Adulto
+            } else {
+                // Idoso
+            }
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >= && idade < 10) {
+                // Criança
+            } else if (idade < 21) {
+                // Jovem
+            } else if (idade < 50) {
+                // Adulto
+            } else {
+                // Idoso
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos um(a) ${genero} com ${idade} anos.`
+     }
+}
