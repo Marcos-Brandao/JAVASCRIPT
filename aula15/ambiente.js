390117890b5626543ac31e022ad149665ff6cdf4
Marcos Brandão <mwellersonbrandao@outlook.com>
2024-01-18 00:21:44 -0300

    atualiazndo ex

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -4,7 +4,7 @@ num.push(1)
 console.log(num)
 console.log(`O vetor tem ${num.length} psições`)
 console.log(`O primeiro valor do vetor é ${num[0]}`)
-let pos = num.indexOf(2)
+let pos = num.indexOf(9)
 if (pos == -1) {
     console.log(`O valor não foi encontrado!`)
 } else {
--- aula16ex/ex/script.js
+++ aula16ex/ex/script.js
@@ -1,11 +1,29 @@
-function gval() {
-    let num = document.getElementById('txtn')
-    let anal = document.getElementById('selanal')
-    if(num.value.length < 1 || num.value.length > 100) {
-        window.alert('Digite um valor entre 1 e 100')
-    } else {
-        anal.innerHTML = ''
+function adcval() {
+    let num = window.document.getElementById('txtn')
+    let ana = document.getElementById('selanal')
+    let n = Number(num.value)
+    let c = []
+    
+    if(n >= 1 && n <= 100) {
+        c.push(n)
+        ana.innerHTML += ''
         let item = document.createElement('option')
-        item.text = `Valor ${num} adicionado`
+        item.text = `Valor ${c} adcionado`
+        ana.appendChild(item)
+        
+    } else {
+        window.alert('Escreva um número entre 1 e 100')
     }
 }
+function fim() {
+    
+
+    
+  }
+for(let res = 0;res < 10;res++) { 
+        console.log(c[res])
+    }
+/*
+
+ 
+*/
